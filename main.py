bca4ead11d0400186836e60407457b310e06882c
Sadokatkhon <sadoqatxonabdullayeva@gmail.com>
2025-02-05 23:22:10 +0500

    Yangi masalalar ishlandi

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Juft yoki toq sonni aniqlash
 # Foydalanuvchidan son kiritishni so‘rang va uning juft yoki toq ekanligini aniqlang.
 
-son = int(input('son kiriting: '))
-if son%2 == 0:
-    print('Juft')
-else:
-    print('Toq')
+# son = int(input('son kiriting: '))
+# if son%2 == 0:
+#     print('Juft')
+# else:
+#     print('Toq')
 
 
 # Fahrenheit → Celsius aylantirish
@@ -23,14 +23,14 @@ else:
 
 
 # Katta yoki kichik sonni aniqlash
-# Ikki son berilgan, kattasini ekranga chiqaring.
-a = int(input('1-son'))
-b = int(input('2-son'))
-
-if a > b:
-    print(a)
-else:
-    print(b)
+# # Ikki son berilgan, kattasini ekranga chiqaring.
+# a = int(input('1-son'))
+# b = int(input('2-son'))
+#
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
 
 # String teskari qilish
 # Foydalanuvchidan so‘z kiritishni so‘rang va uni teskari qilib ekranga chiqaring.
@@ -55,8 +55,70 @@ else:
 # Berilgan n ta Fibonacci sonlarini chiqaruvchi dastur yozing.
 # 0 1 1 2 3 5 8 13
 def fibonacci(n):
-    if n <= 1:
-        return 1
+    pass
 
 
 # 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
+
+# Homework
+# 7. Palindrom so‘zni aniqlash
+#
+# word = input('So\'z kiriting: ')
+# if word == word[::-1]:
+#     print(True)
+# else:
+#     print(False)
+
+# 05.02.2025 kungi dars va masalalar
+
+# 1
+# Satr ichidagi unlilarni sanash
+# Berilgan matndagi barcha unli harflarni sanovchi dastur yozing.
+#
+# unli = ['a','i','u','o','e']
+# text = input('Matn kiriting: ')
+# m = 0
+# for k in text:
+#     if k in unli:
+#         m+=1
+#
+# print(m)
+
+# 2
+# Mukammal sonni aniqlash
+# Berilgan son mukammal son (o‘zidan tashqari bo‘luvchilari yig‘indisi o‘ziga teng) ekanligini tekshiring.
+
+# n = int(input('Bir son kiriting: '))
+# yigindi = 0
+# for i in range(1,n):
+#     if n%i == 0:
+#         yigindi += i
+#
+# if yigindi == n:
+#     print(f' {n} soni mukammal son')
+# else:
+#     print(f' {n} soni mukammal son emas')
+
+# 3
+# Tub sonni aniqlash
+# Berilgan son tub yoki yo‘qligini aniqlovchi funksiya yozing.
+# n = int(input('Bir son kiriting: '))
+# a = 0
+# if n> 0:
+#     for i in range(2,n-1):
+#         if n % i == 0 :
+#             a = 1
+#             break
+# if a == 0:
+#     print(f'{n} tub son')
+# else:
+#     print(f'{n} tub son emas')
+
+
+# 4 Robocontestdan masala
+#
+# a,b = input('Oraliqni kiriting: ').split( )
+# a= int(a)
+# b = int(b)
+# if b >= a:
+#     print('Urinishlar soni:', b-a+1 )
